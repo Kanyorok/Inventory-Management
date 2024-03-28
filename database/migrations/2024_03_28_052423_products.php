1976967ee7bdd3a113ccc7e48a3a3f1a00667ee6
Robert Kanyoro <107240977+Kanyorok@users.noreply.github.com>
2024-03-28 10:11:03 +0300

    Add the relevant migrations for the application

---
+++ database/migrations/2024_03_28_052423_products.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('products', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->BigInteger('product_category_id');
+            $table->Decimal('price', 10, 2);
+            $table->integer('stock')->default(0);
+            $table->integer('stock_defective')->default(0);
+            $table->timestamps();
+            $table->softDeletes();
+            $table->foreign('product_category_id')->references('id')->on('product_categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('products');
+    }
+};
---
+++ database/migrations/2024_03_28_070946_clients.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            $table->decimal('balance')->default(0.00);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('clients', function (Blueprint $table) {
+            $table->dropColumn(['balance']);
+            $table->dropColumn(['last_purchase']);
+            $table->dropColumn(['total_purchases']);
+            $table->dropColumn(['total_paid']);
+        });
+    }
+};
